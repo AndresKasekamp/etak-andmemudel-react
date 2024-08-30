0a03cecc887fd0e3675815ce9552a14d17d09781
Andres Kasekamp <andreskasekamp@gmail.com>
2024-08-30 16:10:47 +0300

    mod

--- src/data/constantFields.tsx
+++ src/data/constantFields.tsx
@@ -1,5 +1,5 @@
 import { Name } from "../types/types";
-import { RowData, headingData } from "../types/interfaces";
+import { RowData } from "../types/interfaces";
 
 export const createData = (
   name: Name,
@@ -10,7 +10,6 @@ export const createData = (
   return { name, dataType, domain, desc };
 };
 
-export const otherMainRows = [{}];
 
 export const otherShapes = [
   {
