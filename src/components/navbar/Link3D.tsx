fbf7c3a12410c6859020ebccc6aad1ba4f7cb6fe
Andres Kasekamp <andreskasekamp@gmail.com>
2024-11-26 10:33:33 +0200

    added buttons with text instead

--- src/components/navbar/Link3D.tsx
+++ src/components/navbar/Link3D.tsx
@@ -1,10 +1,28 @@
 import { Link } from "react-router-dom";
 import ThreeDRotationIcon from "@mui/icons-material/ThreeDRotation";
+import { Button } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 
 const Link3D = () => {
+  const isMobile = useMediaQuery("(max-width:600px)");
+
   return (
     <Link to="/3d" title="3D kihid">
-      <ThreeDRotationIcon fontSize="large" sx={{  color: "white" }} />
+      <Button
+        variant="contained"
+        size="large"
+        startIcon={<ThreeDRotationIcon />}
+        sx={{
+          backgroundColor: "#32774E",
+          borderColor: "#32774E",
+          "&:hover": {
+            backgroundColor: "#285f3f", // Darker shade for hover
+            borderColor: "#285f3f",
+          },
+        }}
+      >
+        {isMobile ? "" : "3D kihid"}
+      </Button>
     </Link>
   );
 };
--- src/components/navbar/LinkDerivatives.tsx
+++ src/components/navbar/LinkDerivatives.tsx
@@ -1,10 +1,28 @@
 import { Link } from "react-router-dom";
 import AltRouteIcon from "@mui/icons-material/AltRoute";
+import { Button } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 
 const LinkDerivatives = () => {
+  const isMobile = useMediaQuery("(max-width:600px)");
+
   return (
     <Link to="/tuletiskihid" title="Tuletiskihid">
-      <AltRouteIcon fontSize="large" sx={{ color: "white" }} />
+      <Button
+        variant="contained"
+        size="large"
+        startIcon={<AltRouteIcon />}
+        sx={{
+          backgroundColor: "#32774E",
+          borderColor: "#32774E",
+          "&:hover": {
+            backgroundColor: "#285f3f", // Darker shade for hover
+            borderColor: "#285f3f",
+          },
+        }}
+      >
+        {isMobile ? "" : "Tuletiskihid"}
+      </Button>
     </Link>
   );
 };
