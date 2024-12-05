23a0b7fbb408091045e4d399137a2bf2f2e54455
Andres Kasekamp <andreskasekamp@gmail.com>
2024-12-05 12:32:43 +0200

    fix about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -42,7 +42,7 @@ const AboutPage = () => {
         <strong>{t("lastUpdated")}:</strong> {__BUILD_DATE__}
       </Typography>
 
-      <Stack direction="row" spacing={1} justifyContent="center" mt={4} >
+      <Stack direction="row" spacing={{ xs: 0.5, sm: 2 }} justifyContent="center" mt={4}  >
         <Button
           variant="outlined"
           startIcon={<GitHub />}
@@ -75,6 +75,7 @@ const AboutPage = () => {
           {t("contact")}
         </Button>
       </Stack>
+
 
       {/* Useful Links Section */}
       <Box sx={{ mt: 6, textAlign: "left" }}>
