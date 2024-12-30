b080b78a019f86fc6ce841c8de4fc871693145c5
Andres Kasekamp <andreskasekamp@gmail.com>
2024-12-30 10:40:25 +0200

    fix

--- src/pages/router/MainWrapper.tsx
+++ src/pages/router/MainWrapper.tsx
@@ -3,23 +3,16 @@ import { Outlet } from "react-router-dom";
 import ScrollToTopOnPageChange from "../../components/helpers/ScrollToTop";
 import ScrollToTop from "react-scroll-to-top";
 import Container from "@mui/material/Container";
-import CssBaseline from "@mui/material/CssBaseline";
-
 
 const MainWrapper = () => {
-
-
-
   return (
     <>
-
       <EtakAppBar />
       <Container maxWidth="xl" sx={{ height: "100vh" }}>
         <ScrollToTopOnPageChange />
         <ScrollToTop smooth color="#6f00ff" />
         <Outlet />
       </Container>
-
     </>
   );
 };
