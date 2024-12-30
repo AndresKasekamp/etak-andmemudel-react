968f1c244d9e86ea0156c4b3ac5cafef336e2ccd
Andres Kasekamp <andreskasekamp@gmail.com>
2024-12-30 09:41:39 +0200

    added baseline

--- src/pages/router/MainWrapper.tsx
+++ src/pages/router/MainWrapper.tsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router-dom";
 import ScrollToTopOnPageChange from "../../components/helpers/ScrollToTop";
 import ScrollToTop from "react-scroll-to-top";
 import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const MainWrapper = () => {
   return (
     <>
+      <CssBaseline />
       <EtakAppBar />
       <Container maxWidth="xl" sx={{ height: "100vh" }}>
         <ScrollToTopOnPageChange />
