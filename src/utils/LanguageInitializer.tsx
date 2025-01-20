faba6616f1e1059413aa0c42c65366240db849cb
Andres Kasekamp <andreskasekamp@gmail.com>
2025-01-20 12:53:55 +0200

    page title change

--- src/utils/LanguageInitializer.tsx
+++ src/utils/LanguageInitializer.tsx
@@ -1,31 +1,8 @@
-import { useEffect, useCallback } from "react";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-
-const useUpdateTextContent = (elementId: string, initialText: string = "") => {
-  useEffect(() => {
-    // Set the initial text when the component mounts
-    const element = document.getElementById(elementId);
-    if (element) {
-      element.textContent = initialText;
-    }
-  }, [elementId, initialText]);
-
-  const updateTextContent = useCallback(
-    (newText: string) => {
-      const element = document.getElementById(elementId);
-      if (element) {
-        element.textContent = newText;
-      }
-    },
-    [elementId]
-  );
-
-  return updateTextContent;
-};
 
 const LanguageInitializer = () => {
   const { i18n } = useTranslation();
-  const updateTitle = useUpdateTextContent("page-title", "ETAK Andmemudel");
 
   const updateLanguage = () => {
     const location = window.location.pathname;
@@ -43,28 +20,17 @@ const LanguageInitializer = () => {
   };
 
   useEffect(() => {
-    const handleLanguageChange = () => {
-      if (i18n.language === "en") {
-        updateTitle("ETAK Data Model");
-      } else {
-        updateTitle("ETAK Andmemudel");
-      }
-    };
-
     // Run the language update on mount
     updateLanguage();
-    handleLanguageChange();
 
     // Add event listener for popstate
     window.addEventListener("popstate", updateLanguage);
-    i18n.on("languageChanged", handleLanguageChange);
 
     // Cleanup event listeners on unmount
     return () => {
       window.removeEventListener("popstate", updateLanguage);
-      i18n.off("languageChanged", handleLanguageChange);
     };
-  }, [i18n, updateTitle]);
+  }, [i18n]);
 
   return null; // No UI, this is just for initialization
 };
