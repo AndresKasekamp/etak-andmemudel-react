f95b1e47f8b25f7d598fe4a54a72e0d5fe513e47
Andres Kasekamp <andreskasekamp@gmail.com>
2025-04-03 10:53:40 +0300

    sorting improve

--- src/utils/sortElements.ts
+++ src/utils/sortElements.ts
@@ -2,9 +2,7 @@ import { orderOfRows } from "../data/orderOfRows";
 import { Field } from "../interfaces/interfaces";
 
 export const sortElements = (elements: Field[]): Field[] => {
-  const sortedElements = elements.sort((a, b) => {
+  return elements.sort((a, b) => {
     return orderOfRows.indexOf(a.meta_type) - orderOfRows.indexOf(b.meta_type);
   });
-
-  return sortedElements;
 };
