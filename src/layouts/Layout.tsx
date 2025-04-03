2f47e50b6ce7ee0297c7025fc6281325e5bb16d7
Andres Kasekamp <andreskasekamp@gmail.com>
2025-04-03 10:42:28 +0300

    improved grouping imports

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,9 @@
 import EtakAppBar from "../components/navbar/EtakAppBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
 import ScrollToTopOnPageChange from "../components/helpers/ScrollToTopOnPageChange";
 import ScrollToTop from "react-scroll-to-top";
 import Container from "@mui/material/Container";
 import NotFoundPage from "../pages/NotFoundPage";
-import { useParams } from "react-router-dom";
 
 const Layout = () => {
   const { lng = "" } = useParams();
